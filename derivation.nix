fb108af5a9428d460dee9d60f276dc758e3bb742
Eugene Shatsky <eugene@shatsky.net>
2025-07-08 13:10:20 +0200

    remove explicit phases from derivation.nix, rely on Makefile

--- derivation.nix
+++ derivation.nix
@@ -6,15 +6,5 @@ stdenv.mkDerivation {
     url = ./.;
   };
   buildInputs = [ sdl3 sdl3-image libexif ];
-  buildPhase = ''
-
-mkdir -p $out/bin
-gcc -DWITH_LIBEXIF -lSDL3 -lSDL3_image -lexif -lm src/viewer.c -o $out/bin/lightning-image-viewer
-
-  '';
-  installPhase = ''
-
-cp -R share $out
-
-  '';
+  makeFlags = [ "PREFIX=$(out)" ];
 }
