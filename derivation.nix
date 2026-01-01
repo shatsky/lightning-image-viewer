d8185dba3da2e0ddab072cebca2821059e6ab7b8
Eugene Shatsky <eugene@shatsky.net>
2026-01-01 16:49:03 +0100

    update derivation.nix to build Rust lib

--- derivation.nix
+++ derivation.nix
@@ -1,11 +1,14 @@
 # cargo libheif-sys uses pkg-config to find libheif
-{ stdenv, sdl3, libheif, cargo, pkg-config }:
+{ rustPlatform, stdenv, sdl3, libheif, cargo, pkg-config }:
 
-stdenv.mkDerivation {
+rustPlatform.buildRustPackage {
   name = "lightning-image-viewer";
-  src = builtins.fetchGit {
-    url = ./.;
-  };
-  buildInputs = [ sdl3 libheif cargo pkg-config ];
-  makeFlags = [ "PREFIX=$(out)" ];
+  src = ./.;
+  # fetch Rust deps before entering offline build env
+  cargoLock.lockFile = ./Cargo.lock;
+  buildInputs = [ sdl3 libheif ];
+  nativeBuildInputs = [ cargo pkg-config ];
+  # default phases are aware only about cargo stuff, of course
+  buildPhase = "make";
+  installPhase = "make install PREFIX=$out";
 }
